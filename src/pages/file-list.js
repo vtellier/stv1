fec25cb802105fa9414c1865c2d681fe1321b2a8
Vincent <vincent.tellier@stingray.no>
2019-01-27 14:00:30 +0100

    Added the source plugin and file list page

---
+++ src/pages/file-list.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+const FileList = ({ data }) => (
+<Layout>
+    <SEO title="FileList" />
+    <h1>File list</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>relativePath</th>
+                <th>prettySize</th>
+                <th>extension</th>
+                <th>birthTime</th>
+            </tr>
+        </thead>
+        <tbody>
+            {data.allFile.edges.map(({ node }, index) => (
+              <tr key={index}>
+                <td>{node.relativePath}</td>
+                <td>{node.prettySize}</td>
+                <td>{node.extension}</td>
+                <td>{node.birthTime}</td>
+              </tr>
+            ))}
+        </tbody>
+    </table>
+    <Link to="/">Go back to the homepage</Link>
+</Layout>
+)
+
+export default FileList
+
+export const query = graphql`
+  query {
+    allFile {
+      edges {
+        node {
+          relativePath
+          prettySize
+          extension
+          birthTime(fromNow: true)
+        }
+      }
+    }
+  }
+`
+
