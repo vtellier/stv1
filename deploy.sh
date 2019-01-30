ae5f5390375e4f9f215529f34164f363a9307ae1
Vincent <vincent.tellier@stingray.no>
2019-01-30 09:47:09 +0100

    Improved deployment script and updated prettier npm package

--- deploy.sh
+++ deploy.sh
@@ -5,9 +5,16 @@ set -o allexport
 [[ -f .env ]] && source .env
 set +o allexport
 
-echo "Will override everything on s3 bucket '$S3_BUCKET'."
+echo "Cleaning cache and building everything from scratch"
+rm -fr .cache public
+gatsby build
 
-aws s3 sync ./public s3://$S3_BUCKET --delete --cache-control max-age=86400
+# Putting a specific robots.txt for stage environment
+echo "User-agent: *" > public/robots.txt
+echo "Disallow: /" >> public/robots.txt
+
+echo "Will override everything on s3 bucket '$S3_BUCKET'."
+aws s3 sync ./public s3://$S3_BUCKET --delete --cache-control max-age=86400 --acl public-read
 
 aws s3 website s3://$S3_BUCKET --index-document index.html --error-document 404.html
 
