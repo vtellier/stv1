e1877c0631f729891b48d680b35738565232e9f7
Vincent <vincent.tellier@stingray.no>
2019-01-31 14:21:44 +0100

    Fixed the language icons

--- src/components/TranslationsMenu.js
+++ src/components/TranslationsMenu.js
@@ -1,22 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import Flag from 'react-country-flag'
+import codes from 'cjs-iso-639'
 
 
-import {
-    IconButton,
-    Menu, MenuItem
-} from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
 import TranslateIcon from '@material-ui/icons/Translate';
 
 export class TranslationsMenu extends React.PureComponent {
-    state = {
-        anchorEl: null,
-    };
     render() {
         let { translations } = this.props;
-        let { anchorEl } = this.state;
-        let opened = Boolean(anchorEl);
 
         if(!translations || translations.length === 0)
             return null;
@@ -26,19 +19,24 @@ export class TranslationsMenu extends React.PureComponent {
             const split = tr.iso3166.split('-');
             if(split.length > 1)
                 tr.iso3166 = split[1];
+            else if(tr.iso3166 === 'EN')
+                tr.iso3166 = 'GB';
+            tr.langName = tr.context.locale;
+            const names = codes['1'][tr.context.locale.toLowerCase()];
+            if(names) {
+                if(names.length > 1)
+                    tr.langName = names[1];
+                else if(names.length > 0)
+                    tr.langName = names[0];
+            }
             return tr;
         });
 
         return (
             <>
-                <IconButton
-                    aria-label="Translations"
-                    color="inherit"
-                >
-                    <TranslateIcon />
-                </IconButton>
+                <TranslateIcon />
                 { translations.map(tr => (
-                    <IconButton key={'translation-'+tr.path} component={Link} to={tr.path}>
+                    <IconButton key={'translation-'+tr.path} component={Link} to={tr.path} title={tr.langName}>
                         <Flag
                             cdnUrl="https://cdnjs.cloudflare.com/ajax/libs/flag-icon-css/3.2.1/flags/4x3/"
                             styleProps={{ width: "24px", height: "24px" }}
