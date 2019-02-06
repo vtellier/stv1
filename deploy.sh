ee1ed52674b67a9482cc86cea2dc63ea9b0d4334
Vincent <vincent.tellier@stingray.no>
2019-02-06 14:40:26 +0100

    fixed the template of recipe list

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,7 @@ echo "User-agent: *" > public/robots.txt
 echo "Disallow: /" >> public/robots.txt
 
 echo "Will override everything on s3 bucket '$S3_BUCKET'."
+aws s3 rm s3://$S3_BUCKET --recursive
 aws s3 sync ./public s3://$S3_BUCKET --delete --cache-control max-age=86400 --acl public-read
 
 aws s3 website s3://$S3_BUCKET --index-document index.html --error-document 404.html
--- src/pages/index.fr.md
+++ src/pages/index.fr.md
@@ -1,7 +1,7 @@
 ---
 title: "Bienvenue!"
 date: "2019-01-29"
-template: "index.js"
+template: "recipe-list.js"
 menuOrder: 1
 menuText: "Accueil"
 menuTitle: "Cliquez ici pour revenir vers la page d'accueil du site"
