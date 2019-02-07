b0ad7439567819c3339bd4d823272e9bf32a2614
Vincent <vincent.tellier@stingray.no>
2019-02-07 10:10:15 +0100

    Removed the duplication of CssBasline

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,15 +6,9 @@
 
 import React from "react"
 import Layout from "./src/components/layout"
-import CssBaseline from '@material-ui/core/CssBaseline';
 
 export const wrapPageElement = ({ element, props }) => {
     // props provide same data to Layout as Page element will get
     // including location, data, etc - you don't need to pass it
-    return (
-        <React.Fragment>
-            <CssBaseline />
-            <Layout {...props}>{element}</Layout>
-        </React.Fragment>
-    );
+    return (<Layout {...props}>{element}</Layout>);
 }
