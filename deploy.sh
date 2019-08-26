bf59e8171c9711e4e0697e8d040d7b2c99f17ec0
Vincent Tellier <vincent.lt.tellier@gmail.com>
2019-08-26 12:13:57 +0200

    Modified the deploy script so it uploads to stage by default

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,10 @@ set -o allexport
 [[ -f .env ]] && source .env
 set +o allexport
 
+S3_BUCKET_TARGET=$S3_BUCKET_STAGE
+
+echo "The project will be deployed to '$S3_BUCKET_TARGET'"
+
 echo "Cleaning cache and building everything from scratch"
 rm -fr .cache public
 gatsby build
@@ -13,9 +17,9 @@ gatsby build
 echo "User-agent: *" > public/robots.txt
 echo "Disallow: /" >> public/robots.txt
 
-echo "Will override everything on s3 bucket '$S3_BUCKET'."
-aws s3 rm s3://$S3_BUCKET --recursive
-aws s3 sync ./public s3://$S3_BUCKET --delete --cache-control max-age=86400 --acl public-read
+echo "Will override everything on s3 bucket '$S3_BUCKET_TARGET'."
+aws s3 rm s3://$S3_BUCKET_TARGET --recursive
+aws s3 sync ./public s3://$S3_BUCKET_TARGET --delete --cache-control max-age=86400 --acl public-read
 
-aws s3 website s3://$S3_BUCKET --index-document index.html --error-document 404.html
+aws s3 website s3://$S3_BUCKET_TARGET --index-document index.html --error-document 404.html
 
